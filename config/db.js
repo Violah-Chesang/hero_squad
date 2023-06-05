dc062238dc310a58c07c41dbb5ce52e797db0603
Violah Chesang <chesangv21@gmail.com>
2023-06-05 12:01:12 +0300

    Created he db configuration file

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+function getDbConnection(){
+
+    const DB_URL = process.env.DB_URL;
+
+    let options = {
+        family : 4
+    };
+
+    mongoose.connect(DB_URL, options);
+
+    const db = mongoose.connection;
+
+    db.on('error', (err) => {
+        console.log('Failed to connect to the server!')
+        console.log(err);
+    });
+
+    db.once('open', (event)=>{
+        console.log('Connection to the server successful!')
+    });
+
+
+
+    return db;
+}
+module.exports = getDbConnection();
