e754c0bb675575d7ba69ceda84bbb648abdef32e
Violah Chesang <chesangv21@gmail.com>
2023-06-06 16:41:05 +0300

    Added other endpoints

--- routes/hero.js
+++ routes/hero.js
@@ -15,13 +15,22 @@ router.get('/hero/find/:heroId', (req,res) => {
 });
 
 //Update a hero - POST/PUT, Hero/update/:heroId, Hero.findOneAndUpdate({id: heroId})
-router.post('hero/update/:heroId', (req,res) => {
+router.post('/hero/update/:heroId', (req,res) => {
     res.json({"message" : `Update hero's information by Id: ${req.body}`})
 });
 
 //delete a hero - POST/DELETE, Hero/delete/:heroId, Hero.findOneAndUpdate({id: heroId})- deleted : true
-router.post('hero/delete/:heroId', (req,res) => {
+router.post('/hero/delete/:heroId', (req,res) => {
     res.json({"message" : `delete a hero by Id: ${req.body.deleted}`})
 });
 
+ //Find a hero's characteristics - GET, Hero/features/:heroId, Hero.find({id: heroId})
+ router.get('/hero/features/:heroId', (req,res) => {
+    res.json({"message" : `Find a hero's characteristics by Id: ${req.params.heroId}`})
+});
+
+//Place a hero in a squad - POST, hero/allocate-squad/:heroId, newHeroSquad.save() 
+router.post('/hero/allocate-squad/:heroId', (req,res) => {
+    res.json({"message" : `Allocate a hero to a squad by Id. Id no. ${req.params.heroId}`})
+});
 module.exports = router
