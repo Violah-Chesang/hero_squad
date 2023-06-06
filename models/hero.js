8e2b00bfc2fd6107c1736683ded770042f7c5af1
Violah Chesang <chesangv21@gmail.com>
2023-06-06 11:46:34 +0300

    Changed the route names

--- models/hero.js
+++ models/hero.js
@@ -4,8 +4,9 @@ let heroSchema = new mongoose.Schema({
     heroId: {type : String, required: true},
     name: {type: String, required: true},
 	age: {type: Number, required: true},
-	strength: {strengthId},
-	weakness: {weaknessId},
+	strength: {type: String}, //StrengthId from Strength module
+	weakness: {type: String}, //weaknessId from Weakness module
+    deleted : {type : Boolean, default: false},
 	assigned: {type: Boolean, default: false},
 	squadId : {type: String},
     weaknessId : {type: String}
--- routes/heroRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const Hero = require('../models/hero');
-
-const heroRouter = express.Router();
-
-
-module.exports = heroRouter
