66d62c6912b3766b72415b5ce7c892f56383e52b
Violah Chesang <chesangv21@gmail.com>
2023-06-07 16:11:55 +0300

    Added route endpoints

--- routes/strength.js
+++ routes/strength.js
@@ -3,5 +3,31 @@ const Strength = require('../models/strength');
 
 const router = express.Router()
 
+// Create a strength - POST, strength/add, newStrength.Save()
+router.post('/strength/add', (req,res) => {
+    res.json({"message" : `Add a new strength`});
+});
 
-modules.exports = router;
+//View all the strength- GET, strength/all, Strength.find({})
+router.get('/strength/all', (req,res) => {
+    res.json({"message" : `View all the strengths`})
+});
+
+ //Find a strength - GET, /strength/find/:strengthId, Strength.find({id: strengthId})
+router.get('/strength/find/:strengthId', (req,res) => {
+    res.json({"message" : `Find a strength by Id: ${req.params.strengthId}`})
+});
+
+//Update a strength - POST/PUT, strength/update/:strengthId, Strength.findOneAndUpdate({id: strengthId})
+router.post('/strength/update/:strengthId', (req,res) => {
+    res.json({"message" : `Update strength's information by Id: ${req.body}`})
+});
+
+//delete a strength - POST/DELETE, /strength/delete/:strengthId, strength.findOneAndUpdate({id: strengthId})- deleted : true
+router.post('/strength/delete/:strengthId', (req,res) => {
+    res.json({"message" : `delete a strength by Id: ${req.body.deleted}`})
+});
+
+
+
+module.exports = router;
