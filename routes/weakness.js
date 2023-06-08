03b060cfdcf7cfb4146c3ec6ea44e4ad1bbc8347
Violah Chesang <chesangv21@gmail.com>
2023-06-08 19:16:01 +0300

    Created CRUD operations for weakness page

--- routes/weakness.js
+++ routes/weakness.js
@@ -12,18 +12,28 @@ router.post('/weakness/add', (req,res) => {
 });
 
 //View all the weaknesses- GET, weakness/all, weakness.find({})
-router.get('/weakness/all', (req,res) => {
-    res.json({"message" : `View all the weaknesses`})
+router.get('/weakness/all',async (req,res) => {
+    const allweaknesses= await Weakness.find({});
+    res.json(allweaknesses);
 });
 
  //Find a weakness - GET, /weakness/find/:weaknessId, weakness.find({id: weaknessId})
-router.get('/weakness/find/:weaknessId', (req,res) => {
-    res.json({"message" : `Find a weakness by Id: ${req.params.weaknessId}`})
+router.get('/weakness/find/:weaknessId',async (req,res) => {
+    const getId = req.params.weaknessId;
+    const id = await Weakness.find({"weaknessId": getId})
+    res.json(id)
 });
 
 //Update a weakness - POST/PUT, weakness/update/:weaknessId, Weakness.findOneAndUpdate({id: weaknessId})
-router.post('/weakness/update/:weaknessId', (req,res) => {
-    res.json({"message" : `Update weakness's information by Id: ${req.body}`})
+router.post('/weakness/update/:weaknessId/:name',async (req,res) => {
+    const filter = {"weaknessId":req.params.weaknessId};
+    const update = {"name" : req.params.name};
+    const options = {
+        new : true
+    }
+
+    const updatedRecord = await Weakness.findOneAndUpdate(filter, update, options);
+    res.json(updatedRecord)
 });
 
 //delete a weakness - POST/DELETE, /weakness/delete/:weaknessId, weakness.findOneAndUpdate({id: weaknessId})- deleted : true
