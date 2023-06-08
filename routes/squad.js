6bf4958b9ccd99933b6ad2c9119a434f0781bf3f
Violah Chesang <chesangv21@gmail.com>
2023-06-08 21:49:36 +0300

    Added a comment

--- routes/squad.js
+++ routes/squad.js
@@ -1,35 +1,53 @@
 const express = require('express');
-const squad = require('../models/squad');
+const Squad = require('../models/squad');
 
 const router = express.Router();
 
 // Create a Squad - POST, squad/add, newSquad.Save()
 router.post('/squad/add', (req,res) => {
-    res.json({"message" : `Add a new Squad with payload: ${req.body}`});
+   const squadBody = req.body;
+
+   const newSquad = new Squad(squadBody);
+    newSquad.save();
+   res.json(newSquad);
 });
 
 //View all the squads- GET, squad/all, Squad.find({})
-router.get('/squad/all', (req,res) => {
-    res.json({"message" : `View all the squads`})
+router.get('/squad/all',async (req,res) => {
+    const allSquads = await Squad.find({});
+    res.json(allSquads);
 });
 
  //Find a squad - GET, squad/find/:squadId, Squad.find({id: squadId})
- router.get('/squad/find/:squadId', (req,res) => {
-    res.json({"message" : `Find a squad by Id. Squad Id is: ${req.params.heroId}`})
+ router.get('/squad/find/:squadId',async (req,res) => {
+    const squadById = await Squad.find({"squadId": req.params.squadId});
+    res.json(squadById);
 });
 
 
 //Update a squad - POST/PUT, squad/update/:squadId, Squad.findOneAndUpdate({id: squadId})
-router.post('/squad/update/:squadId', (req,res) => {
-    res.json({"message" : `Update squad's information by Id. Squad ID: ${req.body}`})
+router.post('/squad/update/:squadId/:cause',async (req,res) => {
+    const filter = {"squadId" : req.params.squadId};
+    const update = {"cause": req.params.cause};
+    const options = { new : true};
+
+    const updatedSquad =  await Squad.findOneAndUpdate(filter,update,options);
+
+    res.json(updatedSquad);
 });
 
 //delete a squad - POST/DELETE, squad/delete/:squadId, Squad.findOneAndUpdate({id: squadId})- deleted : true
-router.post('/squad/delete/:squadId', (req,res) => {
-    res.json({"message" : `delete a squad by Id: ${req.body.deleted}`})
+router.post('/squad/delete/:squadId',async (req,res) => {
+    const filter = {"squadId" : req.params.squadId};
+    const update = {"deleted": true};
+
+    const deletedSquad =  await Squad.findOneAndUpdate(filter,update);
+
+    res.json({"message" : "Record deleted"});
 });
 
  //Find a squad's characteristics - GET, squad/features/:squadId, squad.find({id: squadId})
+ //we need: Id, name, cause, heroes
  router.get('/squad/features/:squadId', (req,res) => {
     res.json({"message" : `Find a squad's characteristics by Id: ${req.params.squadId}`})
 });
