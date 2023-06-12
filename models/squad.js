58d11bcf55ef8a2650bcb137b2c06f517a54caa4
Violah Chesang <chesangv21@gmail.com>
2023-06-12 12:43:30 +0300

    Set the maximum number of heroes to be 5

--- models/squad.js
+++ models/squad.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let squadSchema = new mongoose.Schema({
     squadId: {type : String, required: true},
     name: {type: String, required: true},
-	heroMaxNo: {type: Number, required: true},
+	heroMaxNo: {type: Number, max : 5 ,required: true},
 	cause: {type: String},
     deleted: {type: Boolean, default: false}
 });
